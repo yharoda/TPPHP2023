047e806c6b9196fb1530a7b058adde33cbf5f82c
Carl SCION <yarosajeux@gmail.com>
2023-01-25 15:08:09 +0100

    Third commit

--- tableau3.php
+++ tableau3.php
@@ -1,7 +1,7 @@
 <?php
-$marque = array("volvo","BMW","Toyota", "Peugeot","Renault");
+$marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
 $i=0;
 foreach ($elem as $marque) {
-	echo "Vente de : " . $elem . "<br/>";
+echo "Vente de : " . $elem . "<br/>";
 }
 ?>
