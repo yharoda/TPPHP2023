a0938ab66105a76e35ef63991b10027444e83e06
Titouan van rompaey <116149382+Titouanvanrompaey@users.noreply.github.com>
2023-01-27 10:37:04 +0100

    Update traitement.php

--- PARTIEL/traitement.php
+++ PARTIEL/traitement.php
@@ -2,9 +2,15 @@
 
 require "ressources.php";
 
-$developpeur = array() ;
- 
-$manager = array() ;
+$developpeur = array(new Developpeur(550,"Le Flock", "Louis-Marie",
+"lmleflock@test.net", "0606060606",2500, "Java"),
+new Developpeur(650,"Chellal", "Amine",
+"ach@test.net", "0707070707",2750, "C++"),
+new Developpeur(151,"Katsiev", "Amir",
+"amir@test.net", "0606060606",2750, "Kotlin")) ;
+$manager = array(new Manager(500,"RIPAULT", "Alexis", "alexis@manager.net",
+"0606060606",4000, "DSI")) ; 
+
 
 
 foreach ($developpeur as $value) {
